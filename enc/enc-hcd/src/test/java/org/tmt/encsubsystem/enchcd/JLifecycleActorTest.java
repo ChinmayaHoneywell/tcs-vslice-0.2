0f2ab081fcd5d7f746f5249d019c3e2fe69c7bd5
ashutoshnagdahoneywell <39819411+ashutoshnagdahoneywell@users.noreply.github.com>
2018-06-27 14:31:22 +0530

    junit test cases.

---
+++ enc/enc-hcd/src/test/java/org/tmt/encsubsystem/enchcd/JLifecycleActorTest.java
@@ -0,0 +1,83 @@
+package org.tmt.encsubsystem.enchcd;
+
+import akka.actor.typed.ActorRef;
+import akka.testkit.typed.javadsl.TestKitJunitResource;
+import akka.testkit.typed.javadsl.TestProbe;
+import csw.services.command.scaladsl.CommandResponseManager;
+import csw.services.config.api.javadsl.IConfigClientService;
+import csw.services.config.api.models.ConfigData;
+import csw.services.logging.javadsl.JLoggerFactory;
+import org.junit.*;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import scala.concurrent.duration.Duration;
+
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class JLifecycleActorTest {
+
+    @ClassRule
+    public static final TestKitJunitResource testKit = new TestKitJunitResource();
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Mock
+    CommandResponseManager commandResponseManager;
+    @Mock
+    private IConfigClientService configClientApi;
+    //copy of original configuration for testing
+    String fileLocation = "tcs_test.conf";
+    ConfigData configData;
+
+    JLoggerFactory jLoggerFactory;
+    TestProbe<JStatePublisherActor.StatePublisherMessage> statePublisherMessageTestProbe;
+    ActorRef<JLifecycleActor.LifecycleMessage> lifecycleCmdActor;
+
+    @Before
+    public void setUp() throws Exception {
+        //getting config data from  test resource file to mock actual config service call.
+        URL url = JLifecycleActorTest.class.getClassLoader().getResource(fileLocation);
+        Path filePath = Paths.get(url.toURI());
+        configData = ConfigData.fromPath(filePath);
+
+        jLoggerFactory = new JLoggerFactory("enc-test-logger");
+        statePublisherMessageTestProbe = testKit.createTestProbe();
+        lifecycleCmdActor = testKit.spawn(JLifecycleActor.behavior(commandResponseManager, statePublisherMessageTestProbe.getRef(), configClientApi, jLoggerFactory));
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Given lifecycle actor is created,
+     * when Initialize message is send to lifecycle actor,
+     * then it should load configuration using configuration service,
+     *      tell state publisher actor to start publishing hcd current states
+     *      and mark complete the completableFuture.
+     */
+    @Test
+    public void testOnInitializeMessage() {
+        when(configClientApi.getActive(Paths.get("/org/tmt/tcs/tcs_test.conf"))).thenReturn(CompletableFuture.completedFuture(Optional.of(configData)));
+        CompletableFuture<Void> cf = new CompletableFuture<>();
+        lifecycleCmdActor.tell(new JLifecycleActor.InitializeMessage(cf));
+        statePublisherMessageTestProbe.expectMessage(Duration.create(60, TimeUnit.SECONDS), new JStatePublisherActor.StartMessage());
+        try {
+            cf.get(5, TimeUnit.SECONDS);
+            assertTrue(cf.isDone());
+        } catch (Exception e) {
+            assertTrue(false);
+        }
+    }
+}
