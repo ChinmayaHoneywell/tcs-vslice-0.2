df6748e61f03658ccf75994731f54eefd7361a35
ashutoshnagdahoneywell <ashutosh.nagda@honeywell.com>
2018-12-18 05:11:22 -0500

    csw version 0.6.0

--- pk/project/Libs.scala
+++ pk/project/Libs.scala
@@ -11,7 +11,7 @@ object Libs {
 }
 
 object CSW {
-  val Version = "0.1-SNAPSHOT"
+  val Version = "0.6.0"
 
   val `csw-framework`     = "com.github.tmtsoftware.csw" %% "csw-framework"     % Version
   val `csw-testkit`       = "com.github.tmtsoftware.csw" %% "csw-testkit"     % Version
